deeadac30c1d5fd917d486c37351da67dc38b812
ResulShafili <qiyasogluzefer@gmail.com>
2026-04-18 23:14:15 +0400

    Create exc1.py

---
+++ day_17/exc1.py
@@ -0,0 +1,9 @@
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+finland, sweden, norway, denmark, iceland, es, ru = names
+
+nordic_countries = [finland, sweden, norway, denmark, iceland]
+
+print(nordic_countries)  # ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+print(es)                # Estonia
+print(ru)                # Russia
