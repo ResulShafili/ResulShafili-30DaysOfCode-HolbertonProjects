6d3055cd49d630827d57d0a5d40afcd46d9f653f
ResulShafili <qiyasogluzefer@gmail.com>
2026-04-18 15:13:47 +0400

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,16 @@
+# Day 2: 30 Days of python programming
+# Resul Shafili
+# Single variable declarations
+first_name = "John"
+last_name = "Doe"
+full_name = "John Doe"
+country = "Azerbaijan"
+city = "Baku"
+age = 25
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+
+# Declaring multiple variables on one line
+first_name, last_name, country, city, age = "John", "Doe", "Azerbaijan", "Baku", 25
